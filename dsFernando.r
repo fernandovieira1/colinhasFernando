f2f5d942815540d465c7b3a0c94b3be17ae6f049
Fernando Vieira <fernandovieira@iMac-de-Fernando.local>
2020-11-08 22:17:45 -0400

    Mac 08112020

--- dsFernando.r
+++ dsFernando.r
@@ -137,12 +137,19 @@ head(titan) # Cabeçalho
 tail(titan) # Rodapé
 View(titan) # Imprime todo o dataframe
 nrow(titan) # Nr. de linhas
+length(titan) # Nr. elementos / itens
 colnames(titan) # Nome das colunas
 dim(titan) # nr. linhas e nr. colunas
 length(titan) # Qtde de itens (tamanho do vetor)
 print(titan) # Visão resumida do df -- colunas, nr. linhas e nr. colunas (como tibble, via tidyverse)
+
 colSums(is.na(titan)) # Contagem de dados nulos (linhas), por coluna (CUIDADO: r não lida bem com NaN em variáveis categóricas. A coluna 'Cabin' tem 687 obs (77%) de missing data, mas r não considerou falta de nenhuma)
 round(colMeans(is.na(titan)), 2)
+
+a <- is.na(df)
+a # Procurar por NAs
+df[a] <- 0 # Substituir NAs por 0
+
 summary(titan, digits = 2) # Estatística descritiva do df
 class(titan) # Tipo/classe do objeto
 titan2 <- titan # Cria uma cópia do df
