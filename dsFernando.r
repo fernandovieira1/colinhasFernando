ae485cf8768fdbc87e6bea1b54e175c476ae58e1
Fernando Vieira <fernandovieira@iMac-de-Fernando.local>
2020-11-14 22:19:42 -0400

    mac 14112020 - remover NAs de linhas comm complete.cases

--- dsFernando.r
+++ dsFernando.r
@@ -146,9 +146,12 @@ print(titan) # Visão resumida do df -- colunas, nr. linhas e nr. colunas (como 
 colSums(is.na(titan)) # Contagem de dados nulos (linhas), por coluna (CUIDADO: r não lida bem com NaN em variáveis categóricas. A coluna 'Cabin' tem 687 obs (77%) de missing data, mas r não considerou falta de nenhuma)
 round(colMeans(is.na(titan)), 2)
 
+## Valores ausentes 
 a <- is.na(df)
 a # Procurar por NAs
 df[a] <- 0 # Substituir NAs por 0
+
+df[complete.cases(df),] # mantém apenas as linhas sem NAs (https://statisticsglobe.com/complete-cases-in-r-example/)
 
 summary(titan, digits = 2) # Estatística descritiva do df
 class(titan) # Tipo/classe do objeto
